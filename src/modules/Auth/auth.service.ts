65a8502f51661aefa7888fc804c38d10092bbfdf
nobabsarkar <nobabsarkar2016@gmail.com>
2025-07-06 21:02:22 +0600

    clean code

--- src/modules/Auth/auth.service.ts
+++ src/modules/Auth/auth.service.ts
@@ -21,7 +21,6 @@ const loginUserIntoDB = async (payload: TLoginUser) => {
     email: user?.email,
     mobileNumber: user?.mobileNumber,
     role: user?.role,
-    // status: user.status,
   };
 
   const accessToken = createToken(
